009cf04ab1b1c67204929f65d5154a6e4ec64dff
Meatware Monster <meatwaremonster@gmail.com>
2021-07-07 22:30:13 -0400

    Add basic http service with callback pattern.

---
+++ PlanBuild/Services/HttpService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace PlanBuild.Services
+{
+    class HttpService : MonoBehaviour
+    {
+        private static HttpService _instance;
+
+        public static HttpService Instance { get { return _instance; } }
+
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+
+        public static void SendRequest(UnityWebRequest uwr, System.Action<UnityWebRequest> callback)
+        {
+            Instance.StartCoroutine(SendRequestCoroutine(uwr, callback));
+        }
+
+        private static IEnumerator SendRequestCoroutine(UnityWebRequest uwr, System.Action<UnityWebRequest> callback)
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError)
+            {
+                Jotunn.Logger.LogError("Error While Sending: " + uwr.error);
+            }
+            else
+            {
+                if (uwr.isHttpError)
+                {
+                    Jotunn.Logger.LogError($"Request failed, response code: {uwr.responseCode}");
+                    yield break;
+                }
+                callback(uwr);
+            }
+        }
+    }
+}
---
+++ PlanBuild/Services/ValheimBlueprintStoreService.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.Networking;
+
+namespace PlanBuild.Services
+{
+    class ValheimBlueprintStoreService
+    {
+
+        public static void DeleteBlueprint()
+        {
+            var uwr = UnityWebRequest.Delete("uri");
+            HttpService.SendRequest(uwr, value =>
+            {
+                Jotunn.Logger.LogInfo(value.downloadHandler.text);
+            });
+        }
+
+        public static void DownloadBlueprint()
+        {
+            var uwr = UnityWebRequest.Get("uri");
+            HttpService.SendRequest(uwr, value =>
+            {
+                Jotunn.Logger.LogInfo(value.downloadHandler.text);
+            });
+        }
+
+        public static void UploadBlueprint(Blueprints.Blueprint blueprint)
+        {
+            var uwr = UnityWebRequest.Post("uri", blueprint.ToString());
+            HttpService.SendRequest(uwr, value =>
+            {
+                Jotunn.Logger.LogInfo(value.downloadHandler.text);
+            });
+        }
+
+
+        public static void UpdateBlueprint(Blueprints.Blueprint blueprint)
+        {
+            var uwr = UnityWebRequest.Put("uri", blueprint.ToString());
+            HttpService.SendRequest(uwr, value =>
+            {
+                Jotunn.Logger.LogInfo(value.downloadHandler.text);
+            });
+        }
+    }
+}
